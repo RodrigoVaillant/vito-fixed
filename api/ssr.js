920b5f6e56bb75cea5a648d5ad4f208d546078bc
Complex-Agency-Examples <vpnrodrigovaillant@gmail.com>
2026-05-07 12:20:09 +0300

    Update ssr.js

--- api/ssr.js
+++ api/ssr.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
-    const { default: h } = await import('../dist/server/index.js');
-    return h(req, res);
+    const mod = await import('../dist/server/index.js');
+    res.status(200).send('<pre>' + JSON.stringify(Object.keys(mod), null, 2) + '</pre>');
   } catch (e) {
     res.status(500).send('<pre>' + e.stack + '</pre>');
   }
