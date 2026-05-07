da0958f0f0503b1a5194bdb090e1b2e86e16583d
Complex-Agency-Examples <vpnrodrigovaillant@gmail.com>
2026-05-07 12:25:55 +0300

    Delete api/ssr.js

--- api/ssr.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  try {
-    const mod = await import('../dist/server/index.js');
-    res.status(200).send('<pre>' + JSON.stringify(Object.keys(mod), null, 2) + '</pre>');
-  } catch (e) {
-    res.status(500).send('<pre>' + e.stack + '</pre>');
-  }
-}
